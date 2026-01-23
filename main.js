39bd7f7e1f9446f35042a7f95719bbc1ac843cf7
West Coast Games <67093100+THEGAMER221148@users.noreply.github.com>
2026-01-23 13:24:01 -0800

    p r o g r e s s e d

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Import statements (DO NOT TOUCH THESE UNLESS YOU KNOW WHAT YOU'RE DOING!)
 import DialogueBox from "./engine/DialogueBox.js";
+import Ending from "./engine/Ending.js";
 // Put initialization stuff (charcter definitions, etc.) here!
 
 // These dialogue boxes will be where most of the action happens.
@@ -11,17 +12,18 @@ new DialogueBox("Do you like Fortnite?", "Yes", "No", "white", "Tiny5", 20, () =
     new DialogueBox("Do you want to join my squad?", "Yes!", "No", "white", "Tiny5", 20, () => {
         // Option 1-1: You want to join his squad
         new DialogueBox("Great! I'll be on my Xbox at 5 P.M. See you there!", undefined, undefined, "white", "Tiny5", 20, () => {
-            // TODO: put ending here
+            new Ending("Good Ending", "You've built a stronger friendship with Fortnite Kid.");
         });
     }, () => {
         // Option 1-2: You dont want to join his squad
         new DialogueBox("Ok then, I'll see ya' around!", undefined, undefined, "white", "Tiny5", 20, () => {
-            // TODO: put ending here
+            new Ending("Neutral Ending", "You're still friends with Fortnite Kid.");
         });
     });
     });
 }, () => {
     // Option 2 (u no like Fortnite)
+    new Ending("Bad Ending", "Fortnite Kid killed you instantly. You are now dead.")
 });
 });
 });
